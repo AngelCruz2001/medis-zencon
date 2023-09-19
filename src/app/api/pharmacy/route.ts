eda213ad3677943f0d2b5034a0aa262ac8b5c18f
Gerardo Ceseñas <gcesenasrivera@gmail.com>
2023-09-19 01:59:57 -0300

    Pharmacy backend

--- src/app/api/pharmacy/route.ts
+++ src/app/api/pharmacy/route.ts
@@ -1,20 +1,19 @@
 import connectMongoDB from "@/lib/mongodb";
-import User from "@/models/user.model";
+import Pharmacy from "@/models/pharmacy.model";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
   try {
-    const user = await req.json()
+    const pharmacy = await req.json()
     await connectMongoDB();
-    const info = await User.create(user);
+    const info = await Pharmacy.create(pharmacy);
     return NextResponse.json({
       ok: true,
     });
   } catch (error) {
-    console.log(error)
     return NextResponse.json({
       ok: false,
-      msg: "Error to load the user",
+      msg: `Error to load the pharmacy ${error}`
     });
   }
 }
